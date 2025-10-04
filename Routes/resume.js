6bd6e9c464fe321dd1b80778a15acbaa4db6ae3a
Farhan Hoque <fhrubik98@gmail.com>
2025-10-04 23:28:24 +0600

    vercel issue

--- Routes/resume.js
+++ Routes/resume.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const resumeController = require('../Controllers/resume ');
-const {upload} = require('../utils/multer');
+const resumeController = require('../Controllers/resume');
+const { upload } = require('../utils/multer');
+const connectDB = require('../conn');
 
-router.post('/resume', upload.single('resume'), resumeController.addResume);
-router.get('/get/:user', resumeController.getAllResumeForUser);
+// Add connection handling to routes
+router.post('/resume', upload.single('resume'), async (req, res, next) => {
+  try {
+    await connectDB();
+    await resumeController.addResume(req, res);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/get/:user', async (req, res, next) => {
+  try {
+    await connectDB();
+    await resumeController.getAllResumeForUser(req, res);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
--- Routes/user.js
+++ Routes/user.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const register = require('../Controllers/user');
-const user = require('../Controllers/user');
+const userController = require('../Controllers/user');
+const connectDB = require('../conn');
 
-router.post('/user', register.register);
-router.get('/users', user.getAllUsers);
+router.post('/user', async (req, res, next) => {
+  try {
+    await connectDB();
+    await userController.register(req, res);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/users', async (req, res, next) => {
+  try {
+    await connectDB();
+    await userController.getAllUsers(req, res);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
